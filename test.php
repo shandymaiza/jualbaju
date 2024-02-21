d4c53d393739e3c8370436bc0382dd46e89de5de
shandymaiza <shandymaiza04@gmail.com>
2024-02-21 22:00:09 +0700

    Update Readme, dan penambahan file baru test.php

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contoh Program PHP</title>
+</head>
+<body>
+
+<?php
+    // Variabel untuk menyimpan pesan
+    $pesan = "Halo, ini contoh program PHP!";
+
+    // Cetak pesan ke dalam halaman web
+    echo "<h1>$pesan</h1>";
+?>
+
+</body>
+</html>
+
